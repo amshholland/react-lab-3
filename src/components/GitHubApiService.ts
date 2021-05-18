d878a26be0ef467cc0e0960768d0f601f47a71d5
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-18 08:06:33 -0400

    corrected GHApiService

--- src/components/GitHubApiService.ts
+++ src/components/GitHubApiService.ts
@@ -1,36 +1,30 @@
-import * as React from 'react';
-
 import { Repos, User } from "../model/GitHubApiModel";
 
-const apiKey: string = process.env.REACT_APP_GITHUB_API_CREDENTIALS || "";
+const ghCredentials: string = process.env.REACT_APP_GITHUB_API_CREDENTIALS || "";
+const credentials = btoa( ghCredentials ); // Base 64 endcode
 
-const credentials = btoa( apiKey );
 
-export function fetchUserProfile( username: string ): Promise<User> {
-    // return fetch( `https:api.github.com/users/users${ encodeURIComponent( username ) }`, {
-    //     headers: {
-    //         Authorization: `Basic ${ credentials }`,
-    //     },
-    // } )
-    return fetch( `https://api.github.com/users/${ username }` )
-        .then( ( res ) => res.json() )
+export function fetchUserProfile( name: string ): Promise<User> {
+    return fetch( `https://api.github.com/users/${ name }`, {
+        headers: {
+            Authorization: `Basic ${ credentials }`
+        }
+    } )
+        .then( res => res.json() )
         .then( ( data: User ) => {
             return data;
         } );
 }
-
-export function fetchUserRepos( username: string ): Promise<Repos> {
-    // return fetch( `https:api.github.com/users/users/${ encodeURIComponent( username ) }/repos`, {
-    //     headers: {
-    //         Authorization: `Basic ${ credentials }`,
-    //     },
-    // } )
-    return fetch( `https://api.github.com/users/${ username }/repos` )
+export function fetchUserRepos( name: string ): Promise<Repos[]> {
+    return fetch( `https://api.github.com/users/${ name }/repos`, {
+        headers: {
+            Authorization: `Basic ${ credentials }`
+        }
+    } )
         .then( res => res.json() )
-        .then( ( data: Repos ) => {
+        // The type of data must be the interface for the outermost JSON
+        .then( ( data ) => {
             return data;
         } );
-};
-export { };
-console.log( fetchUserProfile( "amshholland" ) );
-console.log( fetchUserRepos( "amshholland" ) );
+}
+
